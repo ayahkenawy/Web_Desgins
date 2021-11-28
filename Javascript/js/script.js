6fabe7d85276c1d7963f0bf84da3329e7087d672
Aya Ahmed Kenawy <ayahkenawy4490@gmail.com>
2021-11-28 13:18:53 +0200

    Task If

---
+++ Javascript/js/script.js
@@ -0,0 +1,7 @@
+var degree = prompt("Please Enter ur Degree");
+if(degree>100) console.log("Please Enter Avalid Degree");
+else if(degree>=90) console.log("A");
+else if(degree>=80) console.log("B");
+else if(degree>=70) console.log("C");
+else if(degree>=60) console.log("D");
+else console.log("Failed");
