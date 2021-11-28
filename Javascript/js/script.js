e863d3934a61d161d0d481538c73112e07144cfc
Aya Ahmed Kenawy <ayahkenawy4490@gmail.com>
2021-11-28 21:27:00 +0200

    Edit

--- Javascript/js/script.js
+++ Javascript/js/script.js
@@ -5,3 +5,15 @@ else if(degree>=80) console.log("B");
 else if(degree>=70) console.log("C");
 else if(degree>=60) console.log("D");
 else console.log("Failed");
+
+function looping(startNum,endNum,breakNum,continueNum)
+{
+    for(i=startNum;i<=endNum;i++)
+    {
+        debugger
+        if(i==breakNum) break;
+        if(i==continueNum)continue;
+        console.log(i)
+    }
+}
+looping(0,10,8,5)
